9f429929ced22feb803a063efb68bb384a63aa83
VERA-OBIERO <vera.obiero@gmail.com>
2023-11-02 01:05:55 +0300

    persist user data to db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import {useState} from 'react';
 import UserForm from "./components/UserForm"
 
+const BASE_URL ='http://localhost:4000/users'
+
 
 function App () {
+  //useState hook allows us to work with dynamic data inside a component
   const [users, setUsers] = useState([]);
 
+  //save user to db.json
   const handleSaveUser = (user) => {
-    console.log(user)
+    fetch(BASE_URL, {
+      method: 'POST' ,
+      headers : {
+          'Content-Type': 'application/JSON'
+      },
+      body: JSON.stringify(user)
+    })
+      .then((res) => res.json())
+      .then((data) => setUsers([...users, data]))
+      .catch((err) => console.log (err));
   }
 
   return (
     <div className="container mt-5" >
       <UserForm handleSaveUser={handleSaveUser} />
+      <ol>
+        {users.map((user) => <li key={user.id}>{user.firstName}</li>)}
+      </ol>
     </div>
   )
 }
